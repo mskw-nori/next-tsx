a6635e8d0e58d6513a03ae427013068d6f320760
mskw-nori <desudesugi@gmail.com>
2022-04-30 20:59:56 +0900

    update pages

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+import type { NextApiHandler } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+const handler: NextApiHandler = (req, res) => {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default handler
