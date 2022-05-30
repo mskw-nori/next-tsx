febb7c3c325c03daddea3d6efa35ad7e4e23b49b
mskw-nori <desudesugi@gmail.com>
2022-05-30 18:44:53 +0900

    add hooks for swr

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+
+import { usePosts } from '@/hooks/usePosts'
+
+type User = {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+export const Posts: FC = () => {
+  const { data, error, isLoading, isEmpty } = usePosts()
+
+  if (isLoading) {
+    return <div>ローディング中</div>
+  }
+  if (error) {
+    return <div>{error.message}</div>
+  }
+  if (isEmpty) {
+    return <div>データは空です</div>
+  }
+
+  return (
+    <ol>
+      {data.map((post: User) => {
+        const { id, title } = post
+        return <li key={id}>{title}</li>
+      })}
+    </ol>
+  )
+}
