39d4941b5fb783ac5e71cbb633b33a8a2bee9f58
mskw-nori <desudesugi@gmail.com>
2022-06-10 15:04:05 +0900

    SG and fallback prefetch

--- src/pages/comments/[id]/index.tsx
+++ src/pages/comments/[id]/index.tsx
@@ -28,21 +28,28 @@ const UseComment = () => {
 }
 
 export const getStaticPaths = async () => {
-  const comments = await fetch('https://jsonplaceholder.typicode.com/comments')
+  const comments = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=10')
   const commentsData = await comments.json()
   const paths = commentsData.map((comment: any) => ({ params: { id: comment.id.toString() } }))
 
   return {
     paths,
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps = async (ctx: any) => {
   const { id } = ctx.params
   const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`
-  const comments = await fetch(COMMENT_API_URL)
-  const commentData = await comments.json()
+  const comment = await fetch(COMMENT_API_URL)
+
+  if (!comment.ok) {
+    return {
+      notFound: true
+    }
+  }
+
+  const commentData = await comment.json()
 
   return {
     props: {
@@ -55,6 +62,7 @@ export const getStaticProps = async (ctx: any) => {
 
 const CommentsId = (props: any) => {
   const { fallback } = props
+
   return (
     <div className={styles.container}>
       <Head>
